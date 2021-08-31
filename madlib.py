fc648f1c549cd5c88c745f933444ee6b074a01d9
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-08-31 15:02:57 -0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,122 @@
+
+# Homework: Create a madlib. Imagine a story where some of the words are 
+# supplied by user input. Using python you will use input to collect 
+# words for a story and then display the story. 
+
+# Use input to collect each word to a variable
+# Use an f string to display the story
+
+# Your madlib must collect at least 6 words!
+
+
+word_1 = input("Enter a profession: ")
+print("\n")
+word_2 = input("Enter a verb -ing: ")
+print("\n")
+word_3 = input("Enter an adjective: ")
+print("\n")
+word_4 = input("Enter a noun: ")
+print("\n")
+word_5 = input("Enter a verb -ing: ")
+print("\n")
+word_6 = input("Enter a silly word: ")
+print("\n")
+word_7 = input("Enter a body part: ")
+print("\n")
+word_8 = input("Enter a place: ")
+print("\n")
+word_9 = input("Enter a color: ")
+print("\n")
+word_10 = input("Enter a medical procedure: ")
+print("\n")
+word_11 = input("Enter a celebrity name: ")
+print("\n")
+word_12 = input("Enter another celebrity name: ")
+print("\n")
+word_13 = input("Enter an object: ")
+print("\n")
+word_14 = input("Enter a number: ")
+print("\n")
+word_15 = input("Enter a substance: ")
+print("\n")
+word_16 = input("Enter a plural emotion: ")
+print("\n")
+word_17 = input("Enter an adjective: ")
+print("\n")
+word_18 = input("Enter a number: ")
+print("\n")
+word_19 = input("Enter a plural noun: ")
+print("\n")
+word_20 = input("Enter a personality trait: ")
+print("\n")
+
+
+print(f"As a {word_1} with rich experience in {word_2}, I never considered myself\na prime candidate to become an FBI agent. However, it all changed on a {word_3} summer night\nwhen I encountered a flying {word_4} while {word_5} in my backyard. Before I could yell {word_6}, my {word_7}\ngot caught in a bright beam of light, and I was teleported to {word_8}. There, I saw little {word_9}\nbeings performing a {word_10} on someone who looked like {word_11}.\n\n... No, it was {word_12}!")
+print("\n")
+print(f"Without hesitation, I grabbed a {word_13} and attacked them. I must have wounded at least\n{word_14} of the aliens before they retreated, leaving only pools of {word_15} behind.\nI could hear the sirens in the distance. {word_12} was looking at me in absolute {word_16}.")
+print("\n")
+print(f"'Are you okay?' I asked.. ")
+print("\n")
+print(f"{word_12} answered: 'I am {word_17}, thanks!'")
+print(f"At that point, {word_18} government agents burst in, armed with\n{word_19}. Impressed by my {word_20}, they recruited me into the FBI on the spot!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------
+# Partial solution
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = input("Name:")
+# color = input("color:")
+# num = input("Number:")
+
+# print(f"{name} wore {color} shoes while they counted to {num}")
