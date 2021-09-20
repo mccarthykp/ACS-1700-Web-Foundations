b66ab10bc10ca081416522491cfe42d3687b57be
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-09-20 18:38:49 -0400

    organized file structure, changed styled items & updated js

--- mood-shop/script.js
+++ mood-shop/script.js
@@ -1,33 +1,54 @@
-import data from '/resources/data.js'
+import data from '/data.js'
+
+// display moods
 const itemsContainer = document.querySelector('#items')
 
 for (let i = 0; i < data.length; i += 1) {
+    // image & description div
+    const imgDiv = document.createElement('div');
+    imgDiv.className = 'itemImg'
+    itemsContainer.appendChild(imgDiv)
+    
+    
+    // images
 	const newDiv = document.createElement('div');
 	newDiv.className = 'item'
 	const img = document.createElement('img');
-	img.src = data[i].images
+	img.src = data[i].image
 	img.width = 300
 	img.height = 300
-	newDiv.appendChild(img)
-	console.log(img) 
-	itemsContainer.appendChild(newDiv)
 
-    // data
+	imgDiv.appendChild(img)
+	itemsContainer.appendChild(newDiv)
+    newDiv.appendChild(imgDiv)
+
+    // description
     const desc = document.createElement('P')
     desc.innerText = data[i].desc
-    newDiv.appendChild(desc)
-    
+    imgDiv.appendChild(desc)
+
+    // price & button div
+    const dataDiv = document.createElement('div')
+    dataDiv.className = 'itemData'
+
+    itemsContainer.appendChild(dataDiv)
+    newDiv.appendChild(dataDiv)
+
     // price
     const price = document.createElement('P') 
     price.innerText = data[i].price
-    newDiv.appendChild(price)
+
+    dataDiv.appendChild(price)
     
     // button
     const button = document.createElement('button')
     button.id = data[i].name
     button.dataset.price = data[i].price
     button.innerHTML = "Add to Cart"
-    newDiv.appendChild(button)
+
+    dataDiv.appendChild(button)
 
 
 }
+
+// shopping cart
