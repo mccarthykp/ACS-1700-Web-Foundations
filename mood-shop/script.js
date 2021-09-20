3bbe429cafc23b8bbd164beb4584dd936d4e9011
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-09-20 14:43:21 -0400

    displayed moods with button using javascript

---
+++ mood-shop/script.js
@@ -0,0 +1,33 @@
+import data from '/resources/data.js'
+const itemsContainer = document.querySelector('#items')
+
+for (let i = 0; i < data.length; i += 1) {
+	const newDiv = document.createElement('div');
+	newDiv.className = 'item'
+	const img = document.createElement('img');
+	img.src = data[i].image
+	img.width = 300
+	img.height = 300
+	newDiv.appendChild(img)
+	console.log(img) 
+	itemsContainer.appendChild(newDiv)
+
+    // data
+    const desc = document.createElement('P')
+    desc.innerText = data[i].desc
+    newDiv.appendChild(desc)
+    
+    // price
+    const price = document.createElement('P') 
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+    
+    // button
+    const button = document.createElement('button')
+    button.id = data[i].name
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
+
+
+}
