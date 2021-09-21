9f7026012de037702894d0913d6553ee35d2ed90
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-09-20 20:48:39 -0400

    connect cart to footer

--- mood-shop/script.js
+++ mood-shop/script.js
@@ -2,6 +2,11 @@ import data from '/data.js'
 
 // display moods
 const itemsContainer = document.querySelector('#items');
+const itemList = document.getElementById('item-list');
+const cartQty = document.getElementById('cart-qty');
+const cartTotal = document.getElementById('cart-total');
+itemList.innerHTML = '<li> Hello World</li>';
+
 
 for (let i = 0; i < data.length; i += 1) {
     // image & description div
@@ -68,13 +73,23 @@ function addItem(name, price) {
 // show items
 function showItems() {
     const qty = getQty();
-    console.log(`You have ${qty} items in your cart`);
+    // console.log(`You have ${qty} items in your cart`);
+    cartQty.innerHTML = `You have ${qty} items in your cart`;
+
+    let itemStr = '';
 
     for (let i = 0; i < cart.length; i += 1) {
-        console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`);
+        // console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`);
+        // const name = cart[i].name;
+        // const price = cart[i].price;
+        // const qty = cart[i].qty;
+        const {name, price, qty} = cart[i]
+        itemStr += `<li>${name} $${price} x ${qty} = ${qty * price}</li>`;
     }
+    itemList.innerHTML = itemStr;
 
-    console.log(`Total in cart: $${getTotal()}`);
+    // console.log(`Total in cart: $${getTotal()}`);
+    cartTotal.innerHTML = `Total in cart: $${getTotal}`;
 }
 
 // get quantity
