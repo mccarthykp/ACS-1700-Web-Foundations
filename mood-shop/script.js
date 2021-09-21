3ef47ef16a765bae8a21dba753ad3f387e4b5169
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-09-20 20:10:01 -0400

    add item and show item functions now working

--- mood-shop/script.js
+++ mood-shop/script.js
@@ -1,54 +1,64 @@
 import data from '/data.js'
 
 // display moods
-const itemsContainer = document.querySelector('#items')
+const itemsContainer = document.querySelector('#items');
 
 for (let i = 0; i < data.length; i += 1) {
     // image & description div
     const imgDiv = document.createElement('div');
-    imgDiv.className = 'itemImg'
-    itemsContainer.appendChild(imgDiv)
+    imgDiv.className = 'itemImg';
+    itemsContainer.appendChild(imgDiv);
     
     
     // images
 	const newDiv = document.createElement('div');
-	newDiv.className = 'item'
+	newDiv.className = 'item';
 	const img = document.createElement('img');
-	img.src = data[i].image
-	img.width = 300
-	img.height = 300
+	img.src = data[i].image;
+	img.width = 300;
+	img.height = 300;
 
-	imgDiv.appendChild(img)
-	itemsContainer.appendChild(newDiv)
-    newDiv.appendChild(imgDiv)
+	imgDiv.appendChild(img);
+	itemsContainer.appendChild(newDiv);
+    newDiv.appendChild(imgDiv);
 
     // description
-    const desc = document.createElement('P')
-    desc.innerText = data[i].desc
-    imgDiv.appendChild(desc)
+    const desc = document.createElement('P');
+    desc.innerText = data[i].desc;
+    imgDiv.appendChild(desc);
 
     // price & button div
-    const dataDiv = document.createElement('div')
-    dataDiv.className = 'itemData'
+    const dataDiv = document.createElement('div');
+    dataDiv.className = 'itemData';
 
-    itemsContainer.appendChild(dataDiv)
-    newDiv.appendChild(dataDiv)
+    itemsContainer.appendChild(dataDiv);
+    newDiv.appendChild(dataDiv);
 
     // price
-    const price = document.createElement('P') 
-    price.innerText = data[i].price
+    const price = document.createElement('P') ;
+    price.innerText = data[i].price;
 
-    dataDiv.appendChild(price)
+    dataDiv.appendChild(price);
     
     // button
-    const button = document.createElement('button')
-    button.id = data[i].name
-    button.dataset.price = data[i].price
-    button.innerHTML = "Add to Cart"
+    const button = document.createElement('button');
+    button.id = data[i].name;
+    button.dataset.price = data[i].price;
+    button.innerHTML = "Add to Cart";
 
-    dataDiv.appendChild(button)
+    dataDiv.appendChild(button);
 
 
 }
 
 // shopping cart
+const cart = [ ]
+
+function addItem(name, price) {
+    const item = {name:name, price: price, qty: 1}
+    cart.push(name);
+}
+
+function showItems() {
+    console.log(`You have ${cart.length} items in your cart`)
+}
