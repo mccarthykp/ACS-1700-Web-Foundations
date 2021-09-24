7569172a340dee8bab029a428fd59800b05cf2e4
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-09-24 18:55:05 -0400

    changed id names in index.html, created main.js and defined variables for each element in index.html

---
+++ Tip Calculator/main.js
@@ -0,0 +1,7 @@
+const billInput = document.querySelector('#bill')
+const tipInput = document.querySelector('#tip')
+const peopleInput = document.querySelector('#people')
+const displayTip = document.querySelector('#display-tip')
+const displayTotal = document.querySelector('#display-total')
+
+// challenge 5 starts here
